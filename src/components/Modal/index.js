bc98cddf95c32e4a138d488e34544f72a80712c7
Brian <bgswilde@gmail.com>
2021-10-31 14:49:08 -0500

    Set up onClick as a prop to be passed into modals, allowing the modal to be closed on the click of the close button

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 
-function Modal({currentPhoto}) {
+function Modal({ onClose, currentPhoto }) {
     const {name, category, description, index} = currentPhoto;
     
     return (
         <div className="modalBackdrop">
             <div className="modalContainer">
                 <h3 className="modalTitle">{name}</h3>
-                <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
+                <img src={require(`../../assets/large/${category}/${index}.jpg`).default} alt="current category" />
                 <p>
                     {description}
                 </p>
-                <button type="button">
+                <button type="button" onClick={onClose}>
                     Close this modal
                 </button>
             </div>
         </div>
-
     );
 }
+
 
 export default Modal;
